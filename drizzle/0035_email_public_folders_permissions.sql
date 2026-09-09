278c649845eadb1d4938e128c28c6a09a08ad1e9
GinesRico <ginesrico89@gmail.com>
2026-09-09 09:12:13 +0200

    migracion

--- drizzle/0035_email_public_folders_permissions.sql
+++ drizzle/0035_email_public_folders_permissions.sql
@@ -20,6 +20,58 @@ CREATE TABLE "email_user_permissions" (
 
 CREATE INDEX "idx_email_user_permissions_account_user" ON "email_user_permissions" ("account_id","user_id");
 CREATE INDEX "idx_email_user_permissions_target" ON "email_user_permissions" ("mailbox_id","folder_id");
+
+DELETE FROM "email_permissions" p
+USING (
+  SELECT ctid
+  FROM (
+    SELECT
+      ctid,
+      row_number() OVER (
+        PARTITION BY "account_id", "department_id", "mailbox_id"
+        ORDER BY "created_at" DESC, "id" DESC
+      ) AS rn
+    FROM "email_permissions"
+    WHERE "folder_id" IS NULL AND "mailbox_id" IS NOT NULL
+  ) duplicates
+  WHERE rn > 1
+) duplicates
+WHERE p.ctid = duplicates.ctid;
+
+DELETE FROM "email_permissions" p
+USING (
+  SELECT ctid
+  FROM (
+    SELECT
+      ctid,
+      row_number() OVER (
+        PARTITION BY "account_id", "department_id", "folder_id"
+        ORDER BY "created_at" DESC, "id" DESC
+      ) AS rn
+    FROM "email_permissions"
+    WHERE "folder_id" IS NOT NULL
+  ) duplicates
+  WHERE rn > 1
+) duplicates
+WHERE p.ctid = duplicates.ctid;
+
+DELETE FROM "email_folders" f
+USING (
+  SELECT ctid
+  FROM (
+    SELECT
+      ctid,
+      row_number() OVER (
+        PARTITION BY "account_id", "slug"
+        ORDER BY "created_at" DESC, "id" DESC
+      ) AS rn
+    FROM "email_folders"
+    WHERE "mailbox_id" IS NULL
+  ) duplicates
+  WHERE rn > 1
+) duplicates
+WHERE f.ctid = duplicates.ctid;
+
 CREATE UNIQUE INDEX "email_user_permissions_user_mailbox_key" ON "email_user_permissions" ("account_id","user_id","mailbox_id") WHERE "folder_id" IS NULL AND "mailbox_id" IS NOT NULL;
 CREATE UNIQUE INDEX "email_user_permissions_user_folder_key" ON "email_user_permissions" ("account_id","user_id","folder_id") WHERE "folder_id" IS NOT NULL;
 CREATE UNIQUE INDEX "email_permissions_department_mailbox_key" ON "email_permissions" ("account_id","department_id","mailbox_id") WHERE "folder_id" IS NULL AND "mailbox_id" IS NOT NULL;
